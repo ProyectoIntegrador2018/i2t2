4a0af283248c0f16bfe0b28cdd6af7c7980e7b16
Irvel <irvel.nduva@gmail.com>
2019-09-28 23:23:53 -0500

    Fix more missing empty line after magic comments

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
+
 # Main controller for responding to user actions.
 class UsuariosController < ApplicationController
-  def inicio() return end
+  def inicio()
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@
 
 # Base model of a User that can have different roles.
 class User < ApplicationRecord
-
   enum role:  %i[admin_centro_inv admin_plataforma admin_patente
                  admin_cluster emprendedor_base usuario_general]
   after_initialize :set_default_role, if: :new_record?
