ac5c0dba7fd76a43f39c2b9bd7dc82fa2d39e1c1
Adan Villarreal <adanvillarreal0@gmail.com>
2019-10-07 15:26:41 -0500

    Adds required fields for centers.

--- app/models/center.rb
+++ app/models/center.rb
@@ -1,4 +1,8 @@
 class Center < ApplicationRecord
+  validates :full_name, :short_name, :website, :address, :start_date, :building_area,
+    :property_area, :property_private_funding, :property_state_funding,
+    :equipment_private_funding, :equipment_federal_funding, :equipment_state_funding,
+    :director_name, :director_email, :director_phone, :presence => true
   has_and_belongs_to_many :industries
   has_and_belongs_to_many :clusters
   has_many :awards
