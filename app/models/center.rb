2e730a94343ce4f47faf68b2fd4ae018845c5618
Adan Villarreal <adanvillarreal0@gmail.com>
2019-10-14 19:05:43 -0500

    Rubocop corrections.

--- app/models/center.rb
+++ app/models/center.rb
@@ -7,10 +7,10 @@ class Center < ApplicationRecord
             :director_name, :director_email, :director_phone, presence: true
   has_and_belongs_to_many :industries
   has_and_belongs_to_many :clusters
-  has_many :awards, :dependent => :delete_all
-  has_many :equipments, :dependent => :delete_all
-  has_many :idti_areas, :dependent => :delete_all
-  has_many :idti_services, :dependent => :delete_all 
+  has_many :awards, dependent: :delete_all
+  has_many :equipments, dependent: :delete_all
+  has_many :idti_areas, dependent: :delete_all
+  has_many :idti_services, dependent: :delete_all
   accepts_nested_attributes_for :equipments, allow_destroy: true, reject_if: ->(attrs) { attrs['name'].blank? }
   accepts_nested_attributes_for :awards, allow_destroy: true, reject_if: ->(attrs) { attrs['name'].blank? }
   accepts_nested_attributes_for :idti_areas, allow_destroy: true, reject_if: ->(attrs) { attrs['name'].blank? }
