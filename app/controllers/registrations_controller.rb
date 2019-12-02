b9c9d7cc2a74535c5acb2794174a083d9358a4d6
Doigo12 <doigo_nosecomoestas@hotmail.com>
2019-12-02 11:43:32 -0600

    *Se removió el boton de registro de la pagina de inicio a peticion del cliente *Se removio la ruta de acceso al Sign_Up (registro)

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,19 +1,19 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-class RegistrationsController < Devise::RegistrationsController
-  def new
-    super
-  end
+# class RegistrationsController < Devise::RegistrationsController
+#   def new
+#     super
+#   end
 
-  def create
-  	super
-  end
+#   def create
+#   	super
+#   end
 
-  # def update
-    # super
-  # end
+#   # def update
+#     # super
+#   # end
 
-  def sign_up_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :role)
-  end
-end
+#   def sign_up_params
+#     params.require(:user).permit(:email, :password, :password_confirmation, :role)
+#   end
+# end
