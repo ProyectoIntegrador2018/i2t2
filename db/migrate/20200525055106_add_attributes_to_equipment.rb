ad7dff97027c0b10be62b59859f3319a5ac8d2b5
Joao Gabriel Moura <jg.moura.almeida@gmail.com>
2020-05-27 18:39:35 -0600

    Fixes equipment migrations (#105)

--- db/migrate/20200525055106_add_attributes_to_equipment.rb
+++ db/migrate/20200525055106_add_attributes_to_equipment.rb
@@ -1,6 +1,5 @@
 class AddAttributesToEquipment < ActiveRecord::Migration[5.2]
   def change
-  	add_column :equipment, :model, :string
   	add_column :equipment, :brand, :string
   	add_column :equipment, :applications, :string
   end
