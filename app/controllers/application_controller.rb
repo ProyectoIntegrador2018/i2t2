c7cffd4548dcc1b8ff8e9341139ee7d774c566e0
Gerardo Osio <geraosio@gmail.com>
2020-05-30 20:38:58 -0500

    Fix users import through a CSV and other fixes (#110)
    * Remove unused user view
    
    * Add authentication to user's view
    
    * Fix import users from .csv file
    
    * Add explanation message to import users section

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,12 @@
 
 class ApplicationController < ActionController::Base
   # before_action :configure_permitted_parameters, if: :devise_controller?
-  # protected
+
+  protected
+  def authenticate_user!
+    redirect_to :unauthenticated_root, notice: "Debes iniciar sesión para poder ver la página." unless user_signed_in?
+  end
+
   # def configure_permitted_parameters
   #   devise_parameter_sanitizer.for(:sign_up)  { |u| u.permit(  :email, :password, :password_confirmation, roles: [] ) }
   # end
--- app/models/user.rb
+++ app/models/user.rb
@@ -41,108 +41,130 @@ class User < ApplicationRecord
   end
 
   # CSV first-line format for each available import
-  # ONLY USER:    email, role, password, name, organization, job, contact_telephone, office_telephone
-  # CENTER:       ONLY USER params, profile: 'center', full_name, short_name, website, address
-  # CLUSTER:      ONLY USER params, profile: 'cluster', cluster_name
-  # COMPANY:      ONLY USER params, profile: 'company', company_name, industry, reniecyt, location
-  # ENTREPRENEUR: ONLY USER params, profile: 'entrepreneur', organization
-  # RESEARCHER:   ONLY USER params, profile: 'researcher'
+  # USER:         email, role, password, name, organization, job, contact_telephone, office_telephone
+  # CENTER:       USER params, profile: 'center', full_name, short_name, website, address
+  # CLUSTER:      USER params, profile: 'cluster', cluster_name
+  # COMPANY:      USER params, profile: 'company', company_name, industry, reniecyt, location
+  # ENTREPRENEUR: USER params, profile: 'entrepreneur', organization
+  # RESEARCHER:   USER params, profile: 'researcher'
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
       r = row.to_hash
+
       unless r.key?('email') && r.key?('role') && r.key?('password') &&
              r.key?('name') && r.key?('organization') && r.key?('job') &&
              r.key?('contact_telephone') && r.key?('office_telephone')
         raise "Formato de archivo no valido"
       end
+
+      user = nil
       unless User.exists?(email: r['email'])
-        User.create!(email: r['email'],
-                     role: r['role'],
-                     password: r['password'],
-                     password_confirmation: r['password'],
-                     name: r['name'],
-                     organization: r['organization'],
-                     job: r['job'],
-                     contact_telephone: r['contact_telephone'],
-                     office_telephone: r['office_telephone'])
+        user = User.create!(email: r['email'],
+                            role: r['role'],
+                            password: r['password'],
+                            password_confirmation: r['password'],
+                            name: r['name'],
+                            organization: r['organization'],
+                            job: r['job'],
+                            contact_telephone: r['contact_telephone'],
+                            office_telephone: r['office_telephone'])
+      else
+        user = User.find_by_email(r['email'])
       end
 
-      user = User.find(email: r['email'])
+      if r.key?('profile')
+        case r['profile']
+        when 'center'
+          unless r.key?('full_name') && r.key?('short_name') && r.key?('website') && r.key?('address')
+            raise "Formato del archivo con perfil de Centro no es valido"
+          end
 
-      case r.key?('profile')
-      when 'center'
-        unless r.key?('full_name') && r.key?('short_name') && r.key?('website') && r.key?('address')
-          raise "Formato del archivo con perfil de Centro no es valido"
-        end
-
-        unless Center.find(full_name: r['full_name'])
-          center = Center.new(user: user,
-                     full_name: r['full_name'],
-                     short_name: r['short_name'],
-                     website: r['website'],
-                     address: r['address'])
+          center = nil
+          if center = Center.find_by_full_name(r['full_name'])
+            center.user = user
+          else
+            center = Center.new(user: user,
+                                full_name: r['full_name'],
+                                short_name: r['short_name'],
+                                website: r['website'],
+                                address: r['address'],
+                                start_date: Time.now,
+                                director_name: "Importado desde archivo CSV",
+                                director_email: "Importado desde archivo CSV",
+                                director_phone: "Importado desde archivo CSV")
+          end
 
           unless center.save
-            raise "No se pudo guardar el centro #{r['full_name']}"
+            raise "No se pudo guardar el centro #{r['full_name']} para el usuario #{user.name} con id #{user.id}"
           end
-        end
 
-      when 'cluster'
-        unless r.key?('cluster_name')
-          raise "Formato del archivo con perfil de Cluster no es valido"
-        end
+        when 'cluster'
+          unless r.key?('cluster_name')
+            raise "Formato del archivo con perfil de Cluster no es valido"
+          end
 
-        unless Cluster.find(name: r['cluster_name'])
-          cluster = Cluster.new(user: user,
-                     name: r['cluster_name'])
+          cluster = nil
+          if cluster = Cluster.find_by_name(r['cluster_name'])
+            cluster.user_id = user.id
+          else
+            cluster = Cluster.new(user_id: user.id, name: r['cluster_name'])
+          end
 
           unless cluster.save
-            raise "No se pudo guardar el cluter #{r['cluster_name']}"
+            raise "No se pudo guardar el cluter #{r['cluster_name']} para el usuario #{user.name} con id #{user.id}"
           end
-        end
 
-      when 'company'
-        unless r.key?('company_name') && r.key?('industry') && r.key?('reniecyt') && r.key?('location')
-          raise "Formato del archivo con perfil de compañia no es valido"
-        end
+        when 'company'
+          unless r.key?('company_name') && r.key?('industry') && r.key?('reniecyt') && r.key?('location')
+            raise "Formato del archivo con perfil de compañia no es valido"
+          end
 
-        unless Company.find(name: r['company_name'])
-          company = Company.new(user: user,
-                     name: r['company_name'],
-                     industry: r['industry'],
-                     reniecyt: r['reniecyt'],
-                     location: r['location'])
+          company = nil
+          if company = Company.find_by_name(r['company_name'])
+            company.user = user
+          else
+            company = Company.new(user: user,
+                                  name: r['company_name'],
+                                  industry: r['industry'],
+                                  reniecyt: r['reniecyt'],
+                                  location: r['location'])
+          end
 
           unless company.save
-            raise "No se pudo guardar la compañia #{r['company_name']}"
+            raise "No se pudo guardar la compañia #{r['company_name']} para el usuario #{user.name} con id #{user.id}"
           end
-        end
 
-      when 'entrepreneur'
-        unless r.key?('organization')
-          raise "Formato del archivo con perfil de emprendedor no es valido"
-        end
+        when 'entrepreneur'
+          unless r.key?('organization')
+            raise "Formato del archivo con perfil de emprendedor no es valido"
+          end
 
-        unless Entrepreneur.find(organization: r['organization'])
-          entrepreneur = Entrepreneur.new(user: user,
-                     organization: r['organization'])
+          entrepreneur = nil
+          if entrepreneur = Entrepreneur.find_by_organization(r['organization'])
+            entrepreneur.user = user
+          else
+            entrepreneur = Entrepreneur.new(user: user,
+                                            organization: r['organization'])
+          end
 
           unless entrepreneur.save
-            raise "No se pudo guardar el perfil emprendedor de #{r['organization']}"
+            raise "No se pudo guardar el perfil emprendedor de #{r['organization']} para el usuario #{user.name} con id #{user.id}"
           end
-        end
 
-      when 'researcher'
-        unless user.researcher
-          researcher = Researcher.new(user: user)
+        when 'researcher'
+          raise "El usuario #{user.name} con id #{user.id} no tiene el rol de investigador." unless user.investigador?
 
-          unless researcher.save
-            raise "No se pudo guardar el investigador con el usuario #{user.email}"
+          unless user.researcher
+            researcher = Researcher.new(user: user)
+
+            unless researcher.save
+              raise "No se pudo guardar el investigador con el usuario #{user.name} con id #{user.id}"
+            end
           end
-        end
 
-      else
-        raise "Nombre del perfil invalido"
+        else
+          raise "Nombre del perfil invalido para el usuario #{user.name} con id #{user.id}"
+        end
       end
 
     end
@@ -153,9 +175,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one :center
-  has_one :company
-  has_one :researcher
-  has_one :entrepreneur
-  has_one :cluster
+  has_one :center, dependent: :nullify
+  has_one :cluster, dependent: :nullify
+  has_one :company, dependent: :nullify
+  has_one :entrepreneur, dependent: :destroy
+  has_one :researcher, dependent: :destroy
 end
